f21a51f5af36e0b2e3194860844cce4eb3d1987f
shin-05-Ave <f24ba001@chuo.ac.jp>
2026-01-20 21:36:22 +0900

    fix: unify all backgrounds to 1..jpg and bust SW cache

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'tsc-static-v2';
+const ASSETS = [
+  '/',
+  '/TSC/',
+  '/TSC/index.html',
+  '/TSC/css/base.css',
+  '/TSC/css/components.css',
+  '/TSC/css/hero.css',
+  '/TSC/js/ui.js',
+  '/TSC/js/effects.js',
+  '/TSC/js/particles.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {}));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))).catch(() => {})
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(req, clone)).catch(() => {});
+      return res;
+    })).catch(() => fetch(req))
+  );
+});
